dc28ad1d47a98f4ce07de8abd966cf574663d04d
yaochunlin <867674836@qq.com>
2016-04-07 20:45:14 +0800

    创建message控制器

---
+++ banTangCopy/Sections/Message/BTCcMessageController.h
@@ -0,0 +1,13 @@
+//
+//  BTCcMessageController.h
+//  banTangCopy
+//
+//  Created by yaochunlin on 16/4/7.
+//  Copyright © 2016年 LarrySue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BTCcMessageController : UIViewController
+
+@end
---
+++ banTangCopy/Sections/Message/BTCcMessageController.m
@@ -0,0 +1,17 @@
+//
+//  BTCcMessageController.m
+//  banTangCopy
+//
+//  Created by yaochunlin on 16/4/7.
+//  Copyright © 2016年 LarrySue. All rights reserved.
+//
+
+#import "BTCcMessageController.h"
+
+@implementation BTCcMessageController
+
+-(void)viewDidLoad{
+
+    [super viewDidLoad];
+}
+@end
